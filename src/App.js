13cf632a05cff71067324a95dd98d1e740d7a659
rfaelnc <rfaelnc@gmail.com>
2022-08-09 01:26:49 -0300

    initialized

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.handlePClick = this.handlePClick.bind(this);
+
+  //   this.state = {
+  //     name: 'Rafael C',
+  //     counter: 0
+  //   };
+  // }
+
+  state = {
+    name: 'Rafael C',
+    counter: 0
+  };
+  
+  handlePClick = () => {
+    this.setState({ name: 'Jr' });
+  }
+
+  handleAClick = (e) => {
+    e.preventDefault();
+
+    const { counter } = this.state;
+    this.setState({ counter: counter+1 });
+
+  }
+
+  render() {
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+           onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+
+          >
+            Este é o link
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
