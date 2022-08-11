701bd579798e7ff1988ae237d0d441c267d5f67f
rfaelnc <rfaelnc@gmail.com>
2022-08-11 00:31:37 -0300

    props and organization #1

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,15 @@
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts } ) => (
+  <div className="posts">
+    {posts.map((post) => (
+      <PostCard
+        title={post.title}
+        body={post.body}
+        id={post.id}
+        cover={post.cover}
+        // post={post}
+      />
+    ))}
+  </div>
+);
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,29 @@
+import './styles.css';
+
+import { Component } from 'react';
+import { loadPosts } from '../../utils/load-posts';
+import { Posts } from '../../components/Posts';
+
+export class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
+    );
+  }
+}
