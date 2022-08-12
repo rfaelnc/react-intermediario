7a81b78beacf88f18ded60310f2ef177be17d37a
rfaelnc <rfaelnc@gmail.com>
2022-08-12 09:51:41 -0300

    props and organization #1

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,10 +1,11 @@
+import './styles.css';
+
 export const PostCard = ({ title, cover, body, id }) =>  (
     <div key={id} className="post">
       <img src={cover} alt={title} />
       <div className="post-content">
-        <h1>{title}</h1>
+        <h1>{title} {id}</h1>
         <p>{body}</p>
       </div>
     </div>
   );
-  
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -3,10 +3,14 @@ import './styles.css';
 import { Component } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
+import { Button } from '../../components/Button';
 
 export class Home extends Component {
   state = {
-    posts: []
+    posts: [],
+    allPosts:[],
+    page: 0,
+    postsPerPage: 4
   };
 
   async componentDidMount() {
@@ -14,15 +18,45 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos });
+    this.setState(
+      { 
+        posts: postsAndPhotos.slice(page, postsPerPage),
+        allPosts: postsAndPhotos,
+      });
+  }
+
+  loadMorePosts = () => {
+    const{
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage )
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage});
+
   }
 
   render() {
-    const { posts } = this.state;
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
     return (
       <section className="container">
         <Posts posts={posts} />
+        <div className="button-container">
+          <Button 
+          text="Load more posts" 
+          onClick={this.loadMorePosts}
+          disabled={noMorePosts}
+          />
+        </div>
       </section>
     );
   }
